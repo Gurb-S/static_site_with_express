e1b190a226788246d9bbee80cd82893e759d9c9f
Gurb-S <OrganicMintz@gmail.com>
2022-03-22 20:23:22 +0100

    add routes for all three pages

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ app.use(express.urlencoded({extended: false}));
 app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
-const mainRoutes = require('./routes/index');
+const mainRoutes = require('./routes');
+const aboutRuote = require('./routes/about')
 const projectRoute = require('./routes/projects');
 
 app.use(mainRoutes);
-app.use('project',{ projectRoute })
+app.use('/projects',projectRoute)
 
 app.listen(3000, () =>{
     console.log('The app is running on localhost:3000');
