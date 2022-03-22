87a6d41026789765c766ec2d58cf0ab287d63609
Gurb-S <OrganicMintz@gmail.com>
2022-03-22 20:21:16 +0100

    set dynamic routes basics

--- routes/projects.js
+++ routes/projects.js
@@ -2,13 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { projects } = require('../data/data.json');
 
-
-router.get('/:id', (req,res,next) =>{
-    const projectId = req.params.id;
-    const project = projects.find(({ id }) => id === +projectId);
-    if(project){
-        res.render('recipe',{recipe})
-    }
+router.get('/:id',(req,res)=>{
+    const { id } = req.params;
+    console.log(projects[id]);
+    res.render('project', {id})
 })
 
 module.exports = router; 
