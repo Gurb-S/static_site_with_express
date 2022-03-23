754383437613ebccfcd4ae7616154ce194d36af8
Gurb-S <OrganicMintz@gmail.com>
2022-03-23 18:27:07 +0100

    clean up comments

--- routes/projects.js
+++ routes/projects.js
@@ -4,16 +4,12 @@ const { projects } = require('../data/data.json');
 
 router.get(`/:id`,(req,res)=>{
     const { id } = req.params;
-    //console.log(projects[id]);
     const { project_name } = projects[id];
-    //console.log(project_name);
     const { description } = projects[id];
-    //console.log(description)
     const { technologies } = projects[id];
     const { live_link } = projects[id];
     const { github_link } = projects[id];
     const { img_urls } = projects[id]
-    console.log(technologies)
     const templateData = {project_name, description, technologies,live_link,github_link,img_urls}
     res.render('project', templateData)
 })
