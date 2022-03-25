7ffbb0c402ddd67f14ceafe6c56cd426816a2e83
Gurb-S <OrganicMintz@gmail.com>
2022-03-25 22:59:30 +0100

    add 404 handling

--- routes/projects.js
+++ routes/projects.js
@@ -4,14 +4,20 @@ const { projects } = require('../data/data.json');
 
 router.get(`/:id`,(req,res)=>{
     const { id } = req.params;
-    const { project_name } = projects[id];
-    const { description } = projects[id];
-    const { technologies } = projects[id];
-    const { live_link } = projects[id];
-    const { github_link } = projects[id];
-    const { img_urls } = projects[id]
-    const templateData = {project_name, description, technologies,live_link,github_link,img_urls}
-    res.render('project', templateData)
+    console.log(id)
+    if(projects[id] && !isNaN(id)){
+        const { project_name } = projects[id];
+        const { description } = projects[id];
+        const { technologies } = projects[id];
+        const { live_link } = projects[id];
+        const { github_link } = projects[id];
+        const { img_urls } = projects[id]
+        const templateData = {project_name, description, technologies,live_link,github_link,img_urls}
+        res.render('project', templateData)
+    }
+    else{
+        res.redirect('/page-not-found')
+    }
 })
 
 module.exports = router; 
