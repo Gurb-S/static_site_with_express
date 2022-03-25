ecf1262a94d347abf49fea99d3aa714b7a8bcd36
Gurb-S <OrganicMintz@gmail.com>
2022-03-25 22:58:20 +0100

    add error handling

--- app.js
+++ app.js
@@ -15,6 +15,26 @@ app.use(mainRoutes);
 app.use(aboutRuote);
 app.use('/projects',projectRoute)
 
+
+app.use((req,res,next) =>{
+    console.log('404 error handler called')
+    res.status(404).render('page-not-found')
+})
+
+app.use((err,req,res,next)=>{
+    if(err){
+        console.log('Global error handler called', err)
+    }
+    if(err.status === 404){
+        res.status(404).render('page-not-found',{err})
+    }
+    else{
+        err.message = err.message || "Something went wrong";
+        res.status(err.status || 500).render('error', { err });
+    }
+})
+
 app.listen(3000, () =>{
     console.log('The app is running on localhost:3000');
 });
+
